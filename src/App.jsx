087ee5a14e21900dd084e528b295c0bddd5f3590
Abhishek-Kumar <abhishekrana201005@gmail.com>
2021-07-27 18:56:11 +0530

    upto tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl hover:text-blue-500">
+        Hello People,Welcome to tailwind CSS
+      </h1>
+      <button className="border-2 px-1 py-3 hover:bg-transparent hover:border-2 hover:text-gray-800 border-blue-500 rounded-md bg-blue-300 text-white">
+        Hello Tailwind
+      </button>
+    </div>
+  );
+}
+
+export default App;
