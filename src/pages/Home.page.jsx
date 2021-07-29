9b1bbb77269011f4c0f3d60b3a653c6d65fe006f
Abhishek-Kumar <abhishekrana201005@gmail.com>
2021-07-29 13:41:16 +0530

    upto premier component

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // Components
 import EntertainmentCardSlider from "../components/Entertaiment/Entertainmentcard.components";
+import Premier from "../components/Premier/premier.component";
 const HomePage = () => {
   return (
     <>
@@ -10,6 +11,7 @@ const HomePage = () => {
           The best of Entertainment
         </h1>
         <EntertainmentCardSlider />
+        <Premier />
       </div>
     </>
   );
